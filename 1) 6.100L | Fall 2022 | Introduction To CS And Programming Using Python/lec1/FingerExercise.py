e998926b316a12425ffc6a44d6dc8dbb0029b530
ChanMin02 <9657Faker@gmail.com>
2025-02-16 16:03:59 +0900

    Add files via upload

---
+++ 1) 6.100L | Fall 2022 | Introduction To CS And Programming Using Python/lec1/FingerExercise.py
@@ -0,0 +1,15 @@
+"""
+Assume 3 variables are already defined for you:
+a, b, and c. Create a variable called total that adds a and b 
+then multiplies the result by c. 
+
+Include a last line in your code to print the value: print(total)
+"""
+
+a = int(input("What is a?: "))
+b = int(input("What is b?: "))
+c = int(input("What is c?: "))
+
+total = (a + b) * c
+
+print(total)
